1cb4f71c624a7f30944c2b6e4178153a2d57f1d5
rainSax <navisrai@gmail.com>
2022-01-23 13:08:23 -0800

    Created RetractBar class for the first Egypt puzzle
    Also set DefaultPawn_BP Mass in kg to 25

---
+++ Source/CS467_Escape_Room/RetractBar.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
+#include "RetractBar.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class CS467_ESCAPE_ROOM_API URetractBar : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	URetractBar();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	void OpenBar(float dt);
+	void CloseBar(float dt);
+	float TotalMassOfActors() const;
+
+private:
+	float InitialZValue;
+	float CurrentZValue;
+	float FullOpen;
+	float BarLastOpened = 0.f;
+
+	UPROPERTY(EditAnywhere)
+		ATriggerVolume* PressurePlate;
+
+	UPROPERTY(EditAnywhere)
+		float MassToOpenBars = 50.f;
+
+	UPROPERTY(EditAnywhere)
+		float BarCloseDelay = .2f;
+
+	UPROPERTY(EditAnywhere)
+		float BarOpenSpeed = FMath::RandRange(.1f, 2.f);
+
+	UPROPERTY(EditAnywhere)
+		float BarCloseSpeed = FMath::RandRange(.1f, 2.f);
+
+	float LastDoorOpenTime;
+
+	AActor* Owner;
+		
+};
